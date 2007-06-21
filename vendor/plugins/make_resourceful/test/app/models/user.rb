01413ba21808fc822d98eaf0cf31b010da2f245c
epitron <chris@ill-logic.com>
2007-06-21 10:19:39 +0000

    plugins
    git-svn-id: svn://localhost/sandbox@101 54687dfc-5d10-0410-ba0d-89870071e15d

---
+++ vendor/plugins/make_resourceful/test/app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  has_many :things
+
+  before_destroy :ensure_cant_delete_indestructible_user
+
+  protected
+
+  #a completely contrived method to let us test a failed destroy
+  def ensure_cant_delete_indestructible_user
+    false if self.first_name == 'indestructible'
+  end
+  
+end
