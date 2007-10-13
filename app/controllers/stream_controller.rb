ce52046d8dd40ef77cbf9efe8da4d8be74454f42
epitron <chris@ill-logic.com>
2007-10-13 06:03:59 +0000

    upload streaming
    git-svn-id: svn://localhost/sandbox@228 54687dfc-5d10-0410-ba0d-89870071e15d

--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -34,118 +34,152 @@ class StreamController < ApplicationController
         end
     end
 
+=begin
     def stream_file(path, options)
-        raise MissingFile, "Cannot read file #{path}" unless File.file?(path) and File.readable?(path)
+      raise MissingFile, "Cannot read file #{path}" unless File.file?(path) and File.readable?(path)
 
-        options[:length]   ||= File.size(path)
-        options[:filename] ||= File.basename(path)
-        send_file_headers! options
+      options[:length]   ||= File.size(path)
+      options[:filename] ||= File.basename(path)
+      send_file_headers! options
 
-        headers["Last-Modified"] = File.mtime(path).rfc2822 # "Sun, 26 Feb 2006 21:18:23 GMT"
-        headers["Accept-Ranges"] = "bytes"
-        headers["Content-Length"] = options[:length]
-        headers["Content-Type"] = "audio/mpeg"
-        headers["ETag"] = SHA1.new(path).to_s
-        #headers["Connection"] = "close"
-        #headers["Cache-Control"] = "public"
+      headers["Last-Modified"] = File.mtime(path).rfc2822 # "Sun, 26 Feb 2006 21:18:23 GMT"
+      headers["Accept-Ranges"] = "bytes"
+      headers["Content-Length"] = options[:length]
+      headers["Content-Type"] = "audio/mpeg"
+      headers["ETag"] = SHA1.new(path).to_s
+      #headers["Connection"] = "close"
+      #headers["Cache-Control"] = "public"
 
-        @performed_render = false
-        #pp request.env
+      @performed_render = false
+      #pp request.env
 
-	print "opening #{path.inspect}..."
-        file = File.open(path, 'rb')
-        puts "done!"
+      print "opening #{path.inspect}..."
+      file = File.open(path, 'rb')
+      puts "done!"
 
-        if request.env["HTTP_RANGE"] =~ /bytes=(\d+)-(.*)$/ || request.env["HTTP_CONTENT_RANGE"] =~ /bytes (\d+)-(.*)$/
-            file.seek($1.to_i)
-            headers["Content-Range"] = "bytes #{file.pos}-#{options[:length] - 1}/#{options[:length]}"
-            puts "Sending range: #{headers["Content-Range"]}"
-        end
+      if request.env["HTTP_RANGE"] =~ /bytes=(\d+)-(.*)$/ || request.env["HTTP_CONTENT_RANGE"] =~ /bytes (\d+)-(.*)$/
+          file.seek($1.to_i)
+          headers["Content-Range"] = "bytes #{file.pos}-#{options[:length] - 1}/#{options[:length]}"
+          puts "Sending range: #{headers["Content-Range"]}"
+      end
 
-        if options[:stream]
-            render :status => options[:status], :text => Proc.new { |response, output|
-                sent = 0; last_size = 0
-                logger.info "Streaming file #{path}" unless logger.nil?
-                len = options[:buffer_size] || 4096
-                while buf = file.read(len)
-                    output.write(buf)
-                    sent += len
-                    output.flush
-                    print ("\b"*last_size)+"#{sent}"
-                    last_size = sent.to_s.size
-                end
-            }
-        else
-            logger.info "Sending file #{path}" unless logger.nil?
-            render :status => options[:status], :text => file.read
-        end
+      if options[:stream]
+          render :status => options[:status], :text => Proc.new { |response, output|
+              sent = 0; last_size = 0
+              logger.info "Streaming file #{path}" unless logger.nil?
+              len = options[:buffer_size] || 4096
+              while buf = file.read(len)
+                  output.write(buf)
+                  sent += len
+                  output.flush
+                  print ("\b"*last_size)+"#{sent}"
+                  last_size = sent.to_s.size
+              end
+          }
+      else
+          logger.info "Sending file #{path}" unless logger.nil?
+          render :status => options[:status], :text => file.read
+      end
 
-        #pp response.headers
+      #pp response.headers
     end
+=end
 
     def xsendfile(path, options)
-        headers["Content-Transfer-Encoding"] = "binary"
-        headers["Content-Type"] = "application/force-download"
-        headers["Content-Type"] = options[:type] if options[:type]
+      headers["Content-Transfer-Encoding"] = "binary"
+      headers["Content-Type"] = "application/force-download"
+      headers["Content-Type"] = options[:type] if options[:type]
 
-        headers["X-Sendfile"] = path
-        headers["Content-Disposition"] = "attachment; file=\"#{File.basename path}\""
-        #headers["Content-Length"] = File.size(path)
+      headers["X-Sendfile"] = path
+      headers["Content-Disposition"] = "attachment; file=\"#{File.basename path}\""
+      #headers["Content-Length"] = File.size(path)
 
-        # TODO: What does @performed_render change?
-        #@performed_render = false
-        render :nothing => true
+      # TODO: What does @performed_render change?
+      #@performed_render = false
+      render :nothing => true
     end
 
     def track
-        track = Track.find(params[:id])
-        #pp request.env
-        #"HTTP_RANGE"=>"bytes=878672-",
-        #send_file track.fullpath, :type => 'audio/mpeg', :stream => true, :buffer_size => 4096, :disposition => 'inline'
-        #stream_file track.fullpath, :type => 'audio/mpeg', :stream => true, :buffer_size => 4096, :disposition => 'inline'
-        xsendfile track.fullpath, :type => 'audio/mpeg'
+      track = Track.find(params[:id])
+      #pp request.env
+      #"HTTP_RANGE"=>"bytes=878672-",
+      #send_file track.fullpath, :type => 'audio/mpeg', :stream => true, :buffer_size => 4096, :disposition => 'inline'
+      #stream_file track.fullpath, :type => 'audio/mpeg', :stream => true, :buffer_size => 4096, :disposition => 'inline'
+      xsendfile track.fullpath, :type => 'audio/mpeg'
     end
-
-
 
     ### Playlists
 
     def render_playlist
       headers["Content-Type"] = "audio/x-mpegurl; charset=utf-8"
-  		output = []
-  		output << "#EXTM3U"
-  		for track in @tracks
-  			output << "#EXTINF:#{track.length.to_i},#{track.artist.name} - #{track.album.name} - #{track.number}. #{track.title}"
-  			output << "#{url_for :action=>"track", :id=>track.id}.mp3"
-  		end
-  		
+      output = []
+      output << "#EXTM3U"
+      for track in @tracks
+        output << "#EXTINF:#{track.length.to_i},#{track.artist.name} - #{track.album.name} - #{track.number}. #{track.title}"
+        output << "#{url_for :action=>"track", :id=>track.id}.mp3"
+      end
+      
       render :text => output.join("\n")
     end
 
     #after_filter :playlist_filter, :only=>[:album, :artist, :shuffle, :folder]
 
     def album
-        @album = Album.find(params[:id])
-        @tracks = @album.sorted_tracks
-        render_playlist
+      @album = Album.find(params[:id])
+      @tracks = @album.sorted_tracks
+      render_playlist
     end
 
     def artist
-        @artist = Artist.find(params[:id])
-        @tracks = @artist.albums.map(&:sorted_tracks).flatten
-        render_playlist
+      @artist = Artist.find(params[:id])
+      @tracks = @artist.albums.map(&:sorted_tracks).flatten
+      render_playlist
     end
 
     def shuffle
-        num = params[:id].blank? ? 150 : params[:id].to_i
-        num = 400 if num > 400
-        @tracks = Track.find_by_sql( ["SELECT * FROM tracks ORDER BY rand() LIMIT ?", num])
-        render_playlist
+      num = params[:id].blank? ? 150 : params[:id].to_i
+      num = 400 if num > 400
+      @tracks = Track.find_by_sql( ["SELECT * FROM tracks ORDER BY rand() LIMIT ?", num])
+      render_playlist
     end
 
     def folder
-        @tracks = Track.find :all, :conditions=>{:relative_path => params[:relative_path]}, :order=>"relative_path, filename"
-        render_playlist
+      @tracks = Track.find :all, :conditions=>{:relative_path => params[:relative_path]}, :order=>"relative_path, filename"
+      render_playlist
+    end
+    
+    def safe_subdir?(base, sub)
+      path = File.join(base, sub)
+      expanded = File.expand_path(path)
+      # it's a subdir if "base" is a prefix of "expanded"
+      return expanded.index(base) == 0
+    end
+    
+    def uploaded_dir
+      user = User.find params[:user]
+      relative_path = params[:relative_path]
+      base = user.upload_dir
+      raise "Bad mojo!" unless safe_subdir?(base, path)
+      
+      headers["Content-Type"] = "audio/x-mpegurl; charset=utf-8"
+      output = []
+      output << "#EXTM3U"
+      for path in Dir["#{base}/#{relative_path}/**/*.mp3"]
+        relative_filepath = path.gsub("base", '')
+        output << "#EXTINF:-1,#{relative_filepath}"
+        output << "#{url_for :action=>"uploaded_file", :user=>user.id, :relative_filepath=>relative_filepath}.mp3"
+      end
+      
+      render :text => output.join("\n")
+    end
+    
+    def uploaded_file
+      user = User.find params[:user]
+      relative_filepath = params[:relative_filepath]
+      base = user.upload_dir
+      raise "Bad mojo!" unless safe_subdir?(base, relative_filepath)
+      
+      xsendfile File.join(base, relative_filepath), :type => 'audio/mpeg'
     end
 
 end
