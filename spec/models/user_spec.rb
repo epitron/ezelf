a4f65be58bf35e72cfaa2130b64e6fe685e58aa7
epitron <chris@ill-logic.com>
2007-11-20 09:04:05 +0000

    whee
    git-svn-id: svn://localhost/sandbox@267 54687dfc-5d10-0410-ba0d-89870071e15d

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+context "User (in general)" do
+  
+  fixtures :users
+  
+  specify "should create a user from a form" do
+  end
+  
+  specify "should create a user from the console" do
+  end
+  
+end
