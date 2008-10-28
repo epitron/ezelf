eb1c877ecd9407242c0f3a0b6387d700462c16fc
Chris Gahan <chris@ill-logic.com>
2008-10-28 19:09:35 -0400

    * Fixed admin controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,3 +1,5 @@
 class Admin::UsersController < AdminController
-  active_scaffold :users
+  active_scaffold :users do |config|
+    config.columns = [:name, :fullname, :email, :password, :upload_dir, :validated]
+  end
 end
